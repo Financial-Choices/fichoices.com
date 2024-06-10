dc37cae75c8ceeca72a7b8a630bd396c208904c8
Alex Kim <alexkimt@gmail.com>
2024-06-09 22:29:47 -0700

    Update NMLS number

--- src/footer.tsx
+++ src/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <footer id="footer">
                 <h2>Legal Information</h2>
                 <p>
-                    NMLS #1111 - <a _ngcontent-ng-c3758863085="" target="_blank" rel="noopener" href="https://www.nmlsconsumeraccess.org/TuringTestPage.aspx?ReturnUrl=/EntityDetails.aspx/COMPANY/3030"> NMLS consumer access page </a>
+                    NMLS #211465 - <a _ngcontent-ng-c3758863085="" target="_blank" rel="noopener" href="https://www.nmlsconsumeraccess.org/TuringTestPage.aspx?ReturnUrl=/EntityDetails.aspx/COMPANY/211465"> NMLS consumer access page </a>
                     <br />
                     Financial Choices Corp, 4090 Amberfield Cir, Peachtree Corners, GA 30092.
                 </p>
